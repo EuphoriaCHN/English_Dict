97c725062560922a837e31f142411a58ffe5220d
EuphoriaCHN <879969355@qq.com>
2021-06-13 23:56:17 +0800

    feat: 🎸 被 auth 的接口会刷缓存

--- server/app/middleware/auth.ts
+++ server/app/middleware/auth.ts
@@ -10,9 +10,12 @@ export default async function (ctx: Context, next: () => Promise<any>) {
         const tokenData = ctx.helper.verifyJWT(jwtToken as string);
 
         // 缓存对比
-        const tokenInCache = await ctx.app.redis.get(ctx.helper.getLoginRedisKey(tokenData.userID));
+        const redisKey = ctx.helper.getLoginRedisKey(tokenData.userID);
+        const tokenInCache: any = await ctx.app.redis.get(redisKey);
         assert(tokenInCache === jwtToken);
 
+        // 刷新缓存
+        await ctx.app.redis.set(redisKey, tokenInCache, 'EX', global.REDIS_EXPIRE_TIME);
         ctx.jwtUserLoginData = tokenData;
     } catch (err) {
         throw new global.ServerError('NOT_LOGIN', ctx.t('用户未登录'));
