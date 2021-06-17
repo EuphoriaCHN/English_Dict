c340d955f2b56dba6b00e097cf79039bd9484ba8
EuphoriaCHN <879969355@qq.com>
2021-06-17 17:23:42 +0800

    feat: 🎸 header add language selector

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Layout } from 'antd';
 
+import LanguageSelector from '@/components/LanguageSelector';
+
 import './index.scss';
 
 function Header() {
     return (
         <Layout.Header className={'header'}>
-            
+            <LanguageSelector />
         </Layout.Header>
     )
 }
